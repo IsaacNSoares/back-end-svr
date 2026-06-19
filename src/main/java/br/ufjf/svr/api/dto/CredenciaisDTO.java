ef551401a59e6950e92d1d72ebf97e1cc60620af
Isaac Soares <isaac.soares@estudante.ufjf.br>
2026-06-18 22:31:56 -0300

    Inicia implementação de segurança

---
+++ src/main/java/br/ufjf/svr/api/dto/CredenciaisDTO.java
@@ -0,0 +1,11 @@
+package br.ufjf.svr.api.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredenciaisDTO {
+    private String login;
+    private String senha;
+}
---
+++ src/main/java/br/ufjf/svr/model/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.ufjf.svr.model.repository;
+
+import br.ufjf.svr.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByLogin(String login);
+}
